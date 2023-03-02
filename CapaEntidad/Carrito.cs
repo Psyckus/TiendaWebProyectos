933910343c5891e0f2798fbc3a028d7275293fbd
Jonathan Cascante <Jonathan.cz141998@gmail.com>
2023-03-01 20:53:51 -0600

    Fisrt commit

---
+++ CapaEntidad/Carrito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Carrito
+    {
+  
+     public int IdCarrito  { get; set; }
+
+     public Cliente ocliente { get; set; }
+  
+     public Producto oProducto {get; set; }
+
+     public int Cantidad { get; set; }
+
+
+
+
+}
+}
